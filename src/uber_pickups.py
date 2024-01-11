04f0465cad8198714384ed68fd9b46d21b7e50d3
Alberto Moreno González <91873488+albertomorenogonzalez@users.noreply.github.com>
2024-01-11 10:00:34 +0100

    minor problems fixed

--- src/uber_pickups.py
+++ src/uber_pickups.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.write('This is an streamlit app made following this tutorial:) 
-st.write(https://docs.streamlit.io/get-started/tutorials/create-an-app')
+st.write('https://docs.streamlit.io/get-started/tutorials/create-an-app')
 
 st.title('Uber pickups in NYC')
 
